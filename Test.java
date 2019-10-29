fcc681985cef6dfb839f858de0fc4466d1e1c33f
Dr. Ray Klump <klumpra@lewisu.edu>
2019-10-29 18:57:41 -0500

    yet another glorious change

--- Test.java
+++ Test.java
@@ -1,6 +1,7 @@
 public class Test {
 	public static void main(String[] args) {
-		System.out.println("Hello");
 		System.out.println("added in dev again");
+		System.out.println("added in dev yet again.);
+
 	}
 }
