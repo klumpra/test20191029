ff409ad00a1ad8b180e94b813b99aaef7e8c98d9
Dr. Ray Klump <klumpra@lewisu.edu>
2019-10-29 19:02:31 -0500

    Made some local changes.

--- Test.java
+++ Test.java
@@ -1,7 +1,8 @@
 public class Test {
 	public static void main(String[] args) {
 		System.out.println("added in dev again");
-		System.out.println("added in dev yet again.);
+		System.out.println("added in dev yet again. It is cool);
+		System.out.println("Added locally.");
 
 	}
 }
